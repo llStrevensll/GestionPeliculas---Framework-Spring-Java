bc5fb45ceeffcea3978c3912be34ffd61e99d7d5
Steven <anguelr12@hotmail.com>
2020-01-22 11:25:08 -0500

    Spring Data JPA Configuracion

---
+++ src/main/java/pruebasjpa/AppConexion.java
@@ -0,0 +1,16 @@
+package pruebasjpa;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppConexion {
+
+	public static void main(String[] args) {
+		
+		//Instancia de application context - config de los beans: root-context.xml
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("root-context.xml");
+
+		
+		context.close();
+	}
+
+}
