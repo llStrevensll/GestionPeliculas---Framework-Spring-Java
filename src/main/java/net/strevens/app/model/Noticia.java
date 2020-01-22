6f17825baf1d806ad14de1330a87a45d7c713200
Steven <anguelr12@hotmail.com>
2020-01-22 11:31:46 -0500

    Anotaciones JPA

--- src/main/java/net/strevens/app/model/Noticia.java
+++ src/main/java/net/strevens/app/model/Noticia.java
@@ -2,7 +2,18 @@ package net.strevens.app.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="noticias")
 public class Noticia {
+	//Id, generar autoincrement valor -> GeneratedValue -- Mysql usa constante IDENTITY
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String titulo;
 	private Date fecha;
